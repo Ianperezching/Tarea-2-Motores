9958b45454247dbaa4096730c5a90d16ba11fb1c
unknown <Ian.marcelo.perez@gmail.com>
2024-10-13 22:32:39 -0500

    Menu Con Sonido/ Controlador De Sonido

---
+++ 3C26_Motores_PA2-main/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Button btnPlay;
+    public GameObject SonidoMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnPlay.onClick.AddListener(() => Play());
+        audioSource.Play();
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void About()
+    {
+        SonidoMenu.SetActive(true);
+    }
+    public void Aboutclose()
+    {
+        SonidoMenu.SetActive(false);
+    }
+}
