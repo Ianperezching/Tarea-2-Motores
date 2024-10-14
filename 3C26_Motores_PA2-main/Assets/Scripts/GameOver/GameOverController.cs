c96a4024eb78f29f755467cf0daacd6ba55b0a7c
unknown <Ian.marcelo.perez@gmail.com>
2024-10-13 23:00:18 -0500

    sonido Game Over

--- 3C26_Motores_PA2-main/Assets/Scripts/GameOver/GameOverController.cs
+++ 3C26_Motores_PA2-main/Assets/Scripts/GameOver/GameOverController.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class GameOverController : MonoBehaviour
 {
     public Button btnPlay;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         btnPlay.onClick.AddListener(() => ReturnMenu());
+        audioSource.Play();
     }
 
     void ReturnMenu()
